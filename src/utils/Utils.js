da46a975a0b8fb7ee43ca189aeacd327595e899b
jasonChen1982 <jasonchen1882@gmail.com>
2018-01-31 10:48:17 +0800

    feat(utils): remove some redundant function

--- src/utils/Utils.js
+++ src/utils/Utils.js
@@ -13,71 +13,6 @@ function _rt(val) {
  * @namespace Utils
  */
 export const Utils = {
-
-  /**
-   * determine whether it is a `Array`
-   *
-   * @static
-   * @method
-   * @memberof Utils
-   * @param {*} variable a variable which you want to determine
-   * @return {Boolean} type result
-   */
-  isArray: (function() {
-    const ks = _rt([]);
-    return function(variable) {
-      return _rt(variable) === ks;
-    };
-  })(),
-
-  /**
-   * determine whether it is a `Object`
-   *
-   * @static
-   * @method
-   * @memberof Utils
-   * @param {*} variable a variable which you want to determine
-   * @return {Boolean} type result
-   */
-  isObject: (function() {
-    const ks = _rt({});
-    return function(variable) {
-      return _rt(variable) === ks;
-    };
-  })(),
-
-  /**
-   * determine whether it is a `String`
-   *
-   * @static
-   * @method
-   * @memberof Utils
-   * @param {*} variable a variable which you want to determine
-   * @return {Boolean} type result
-   */
-  isString: (function() {
-    const ks = _rt('s');
-    return function(variable) {
-      return _rt(variable) === ks;
-    };
-  })(),
-
-  /**
-   * determine whether it is a `Number`
-   *
-   * @static
-   * @method
-   * @memberof Utils
-   * @param {*} variable a variable which you want to determine
-   * @return {Boolean} type result
-   */
-  isNumber: (function() {
-    const ks = _rt(1);
-    return function(variable) {
-      return _rt(variable) === ks;
-    };
-  })(),
-
   /**
    * determine whether it is a `Function`
    *
@@ -107,20 +42,4 @@ export const Utils = {
   isUndefined(variable) {
     return typeof variable === 'undefined';
   },
-
-  /**
-   * determine whether it is a `Boolean`
-   *
-   * @static
-   * @method
-   * @memberof Utils
-   * @param {*} variable a variable which you want to determine
-   * @return {Boolean} type result
-   */
-  isBoolean: (function() {
-    const ks = _rt(true);
-    return function(variable) {
-      return _rt(variable) === ks;
-    };
-  })(),
 };
